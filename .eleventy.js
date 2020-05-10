ea19ad05c4b795a4e31abf6f69ba1ea5831ca831
Hélio Correia <dev@heliocorreia.com>
2020-05-10 23:24:56 +0100

    Add posts collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const moment = require('moment');
+const collections = require('./src/_includes/js/collections.js');
+
 moment.locale('en');
 
 module.exports = function (eleventyConfig) {
@@ -6,6 +8,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter('dateformat', function (timestamp) {
         return moment(timestamp).format('MMM DD, YYYY [at] h:mm a');
     });
+
+    // Collections
+    eleventyConfig.addCollection('postsCollection', collections.postsOrderedByDateDesc);
 
     return {
         dir: {
